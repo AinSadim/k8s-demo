5d058bf4cc3d503aeeca17d8fd36b938350dd75f
datlt <datlt.uit@gmail.com>
2021-11-19 13:40:02 +0700

    Hotfix env

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,29 +1,10 @@
 import { difference, isEmpty } from 'lodash';
 import mongoose from 'mongoose';
 import { app } from './app';
-import { permissions } from './constant/auth0';
-import { Auth0 } from './services/auth0';
 import { MONGO_URI, PORT } from './utils/secrets';
-
-async function syncPermission() {
-  let scopes = (await Auth0.shared.getLotoResourceServer()).scopes ?? [];
-  const addedPermissions = scopes.map((s) => s.value);
-  const permissionsNeedAdd = difference(permissions, addedPermissions);
-
-  if (isEmpty(permissionsNeedAdd)) return;
-
-  // * Add missing permissions
-  scopes = scopes.concat(permissionsNeedAdd.map((p) => ({ value: p, description: p })));
-  console.info('Add missing permissions', permissionsNeedAdd);
-  await Auth0.shared.updateLotoPermissions(scopes);
-  console.info('Add missing permissions successful');
-}
 
 const start = async () => {
   console.log('Auth services starting...');
-
-  // * Synchronize permissions
-  syncPermission().catch();
 
   try {
     await mongoose.connect(MONGO_URI, {
